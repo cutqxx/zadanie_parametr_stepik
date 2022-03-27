c283cd61ad671b893c04056b8e48c52d4bb17778
cutqxx <cutqxx@gmail.com>
2022-03-27 15:54:46 +0300

    add sleep in test_items.py

--- test_items.py
+++ test_items.py
@@ -1,8 +1,12 @@
+import time
+
+
 def test_find_button(browser):
     #correct link:
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     # not correct lenk:
     # link = "http://selenium1py.pythonanywhere.com/ru/"
     browser.get(link)
+    time.sleep(30)
     assert browser.find_elements_by_css_selector("button.btn-add-to-basket"), "Button not find!"
 
