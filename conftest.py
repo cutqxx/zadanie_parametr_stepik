259770ec28df09cde7acde7c1778aac01f596a76
cutqxx <cutqxx@gmail.com>
2022-03-27 16:02:28 +0300

    correct

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 # передача параметров в командной строке
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default="ru",
