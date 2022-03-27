6d816be71312f96e18812bf61f3080598e583ff8
cutqxx <cutqxx@gmail.com>
2022-03-27 16:04:31 +0300

    correct

--- test_items.py
+++ test_items.py
@@ -3,9 +3,9 @@ import time
 
 def test_find_button(browser):
     #correct link:
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     # not correct link:
-    link = "http://selenium1py.pythonanywhere.com/ru/"
+    # link = "http://selenium1py.pythonanywhere.com/ru/"
     browser.get(link)
     time.sleep(30)
     assert browser.find_elements_by_css_selector("button.btn-add-to-basket"), "Button not find!"
